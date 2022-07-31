cc7a96c959faf7f816f93e365b99e35df461cee1
Ela NASIR <98895503+ELASY313@users.noreply.github.com>
2022-07-31 18:04:04 +0300

    Add files via upload

---
+++ src/components/MenüTarihçe.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+//Tercih Edilen Yemekleri belirleyip tarihçesi yazdım
+
+const MenüTarihçe = () => {
+  return (
+    <div>
+        
+      <h1>Ünlü Yemeklerimizin Tarihçesi</h1>
+      <h3> general tavuk</h3>
+      <p> 
+      adını ünlü çin generali general tso tsungtangdan alan ama general tso ile bağlantısı net olarak bilinmeyen müthiş lezzetli çin yemeği. yemek 1970lerde amerikada ünlenmeye başlamış general tso ise 1812-1885 yıllarında yaşamış bir komutan. bir rivayete göre generalin karısı askeri zaferlerden sonra bu yemeği yaparmış </p>
+       
+
+
+       
+        <h3>Humburger</h3>
+        <p>  Hamburgerin tarihi Orta Asya'ya, Tatar diye bilinen Türk toplumlarına kadar uzanır. O zamanlar savaşçı Tatarlar sürekli atları üzerinde savaşıyor ve yiyecek olarak yanlarında çiğ et taşıyorlardı. Zamanla eğerlerinin altına koydukları etin, atın hareketleri sonucu sürtünmeden ve atın vücut sıcaklığından dolayı  az da olsa piştiğini ve daha kolay çiğnenebilir hale geldiğini keşfettiler.</p>
+        
+
+        <h3>Döner</h3>
+        <p> Dönerin kebap olarak ilk defa Bursa’da İskender Bey tarafından icat edildiği söyleniyor mesela, Aşağı yukarı 150 sene önce kuzu çevirme yemeğinin yapım tekniğinden yararlanarak kemiksiz kuzu ve dana etlerinin şişe takılması ve ateş karşısında pişirmesi ile ortaya çıktığı vurgulanıyor.</p>
+    
+    
+    </div>
+  )
+}
+
+export default MenüTarihçe
