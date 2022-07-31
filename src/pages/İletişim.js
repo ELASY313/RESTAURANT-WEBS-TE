d19dca4c5952d03d4db3d44388520be12442e3c4
Ela NASIR <98895503+ELASY313@users.noreply.github.com>
2022-07-31 18:24:19 +0300

    Update İletişim.js

--- src/pages/İletişim.js
+++ src/pages/İletişim.js
@@ -3,6 +3,11 @@ import React from 'react'
 const İletişim = () => {
   return (
     <div>
+       <h2> Adress</h2>
+      <p>Kızılırmak Mah. 1450 Sk., ATM Plaza B Blok 1-G Çankaya 06510 Ankara</p>
+
+      <h3> Telefon</h3>
+      <p>0(312) 220 00 46</p>
       
     </div>
   )
