e4a1bc8dbdac563d0aff857bb9a0070b8e2e5576
Ela NASIR <98895503+ELASY313@users.noreply.github.com>
2022-07-31 18:08:48 +0300

    Update Menü.js

--- src/pages/Menü.js
+++ src/pages/Menü.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import Data from './../components/Data';
 
- {/* map ile tüm data bilgillere geçtip item değişkene kaydetttim sonra div kısmına bilgileri */}
+ {/* map ile tüm data bilgillere geçtip item değişkene kaydetttim */}
 
 const Menü = () => {
    
 
   const blogitem = Data.map((item)=>{
     return(
-<div  className = 'col-md-2'> 
-<img src ={item.img}/>
-<h5>{item.title}</h5>
-<h6>{item.price}</h6>
+<div>
+    <div  className = 'col-md-2'> 
+    <img src ={item.img}/>
+    <h5>{item.title}</h5>
+     <h6>{item.price}</h6>
+   
   
+    </div>
+ <button> <a href='#'></a>Hemen Sipariş Ver</button>
 </div>
-
 
     )
 
