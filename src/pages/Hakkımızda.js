272f550430497b12ad9a217c346ddf2fc4ef8cad
Ela NASIR <98895503+ELASY313@users.noreply.github.com>
2022-07-31 18:21:28 +0300

    Update Hakkımızda.js

--- src/pages/Hakkımızda.js
+++ src/pages/Hakkımızda.js
@@ -3,6 +3,19 @@ import React from 'react'
 const Hakkımızda = () => {
   return (
     <div>
+    
+    <h1>
+
+        Hakkımızda
+      </h1>
+
+      <p> Eşsiz uzakdoğu lezzetlerini usta ellerin beceri ve tecrübesi ile birleştirip, Ankara'nın en iyi Sushilerini sizler için servis. 
+
+           Ankaralılar için yepyeni bir uzakdoğu lezzetleri alternatifi var.
+
+           Chef Izakaya ,Ateş, Ustalık ve Lezzet.
+      </p>
+      
       
     </div>
   )
